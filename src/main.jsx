d7a2e55786657e21e39a854a06cb3ef634e78d4f
Jaskaransingh098 <jaskarandosanjh88@gmail.com>
2025-05-26 16:10:35 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
+// import { ToastContainer } from "react-toastify";
 import App from "./App";
 import "./main.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <ToastContainer
+    {/* <ToastContainer
       position="top-right"
       autoClose={3000}
       hideProgressBar={false}
@@ -20,6 +20,6 @@ createRoot(document.getElementById("root")).render(
       draggable
       pauseOnHover
       theme="colored"
-    />
+    /> */}
   </StrictMode>
 );
