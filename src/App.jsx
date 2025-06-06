4612c40aa2ef7613495e415f6b80fb3977001c7e
Jaskaransingh098 <jaskarandosanjh88@gmail.com>
2025-06-06 23:27:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Prices from "./Pages/Prices/Prices";
 import Success from "./Pages/Success/Success";
 import Government from "./Pages/Government/Government";
 import AnimatedBackground from "./Components/AnimateBackground/AnimateBackground";
+import "./App.css"
 
 
 function AppContent() {
